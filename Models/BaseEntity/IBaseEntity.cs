1ad078f1e597352abeb0a225ad4984087cf57d39
Razvan-Gabriel Visinica <razvan.visinica@s.unibuc.ro>
2022-01-22 15:10:47 +0200

    first commit

---
+++ Models/BaseEntity/IBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeForCoffee.Models.BaseEntity
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+
+        DateTime? DateCreated { get; set; }
+
+        DateTime? DateModified { get; set; }
+    }
+}
